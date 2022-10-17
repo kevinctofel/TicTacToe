07827f0927497404e1a7b4f6d1face80f6373d8c
Kevin C. Tofel <kevin@kctofel.com>
2022-10-17 18:17:21 -0400

    Loop through all moves

--- TicTacToe.js
+++ TicTacToe.js
@@ -9,6 +9,8 @@ function prompt(msg) {
 }
 
 function displayBoard(board) {
+  console.clear();
+
   console.log('');
   console.log('     |     |');
   console.log(`  ${board['1']}  |  ${board['2']}  |  ${board['3']}`);
@@ -38,6 +40,14 @@ function emptySquares(board) {
   return Object.keys(board).filter(key => board[key] === INITIAL_MARKER);
 }
 
+function boardFull(board) {
+  return emptySquares(board).length === 0;
+}
+
+function someoneWon(board) { // board is unused for now; we'll use it later
+  return false;
+}
+
 function playerChoosesSquare(board) {
   let square;
 
@@ -62,7 +72,10 @@ function computerChoosesSquare(board) {
 let board = initializeBoard();
 displayBoard(board);
 
-playerChoosesSquare(board);
-computerChoosesSquare(board);
+while (true) {
+  playerChoosesSquare(board);
+  computerChoosesSquare(board);
+  displayBoard(board);
 
-displayBoard(board);
+  if (someoneWon(board) || boardFull(board)) break;
+}
