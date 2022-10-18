959a84c70a2b987662659749edff4996b53e099f
Kevin C. Tofel <kevin@kctofel.com>
2022-10-17 21:14:48 -0400

    Complete

--- TicTacToe.js
+++ TicTacToe.js
@@ -8,8 +8,38 @@ function prompt(msg) {
   console.log(`=> ${msg}`);
 }
 
+function detectWinner(board) {
+  let winningLines = [
+    [1, 2, 3], [4, 5, 6], [7, 8, 9], // rows
+    [1, 4, 7], [2, 5, 8], [3, 6, 9], // columns
+    [1, 5, 9], [3, 5, 7]             // diagonals
+  ];
+
+  for (let line = 0; line < winningLines.length; line++) {
+    let [ sq1, sq2, sq3 ] = winningLines[line];
+
+    if (
+        board[sq1] === HUMAN_MARKER &&
+        board[sq2] === HUMAN_MARKER &&
+        board[sq3] === HUMAN_MARKER
+    ) {
+      return 'Player';
+    } else if (
+        board[sq1] === COMPUTER_MARKER &&
+        board[sq2] === COMPUTER_MARKER &&
+        board[sq3] === COMPUTER_MARKER
+    ) {
+      return 'Computer';
+    }
+  }
+
+  return null;
+}
+
 function displayBoard(board) {
   console.clear();
+
+  console.log(`You are ${HUMAN_MARKER}. Computer is ${COMPUTER_MARKER}`);
 
   console.log('');
   console.log('     |     |');
@@ -45,7 +75,7 @@ function boardFull(board) {
 }
 
 function someoneWon(board) { // board is unused for now; we'll use it later
-  return false;
+  return !!detectWinner(board);
 }
 
 function playerChoosesSquare(board) {
@@ -69,13 +99,32 @@ function computerChoosesSquare(board) {
   board[square] = COMPUTER_MARKER;
 }
 
-let board = initializeBoard();
-displayBoard(board);
 
 while (true) {
-  playerChoosesSquare(board);
-  computerChoosesSquare(board);
-  displayBoard(board);
+  let board = initializeBoard();
+  
+  while(true) {
 
+  displayBoard(board);
+  playerChoosesSquare(board);
   if (someoneWon(board) || boardFull(board)) break;
+  computerChoosesSquare(board);
+  if (someoneWon(board) || boardFull(board)) break;
+  displayBoard(board);
 }
+
+displayBoard(board);
+
+if (someoneWon(board)) {
+  prompt(`${detectWinner(board)} won!`);
+} else {
+  prompt("It's a tie!");
+}
+
+prompt('Play again? (y or n)');
+let answer = readline.question().toLowerCase()[0];
+if (answer !== 'y') break;
+}
+
+
+prompt('Thanks for playing Tic Tac Toe!');
