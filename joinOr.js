64b22c5266a36487ec36e9c340b8d6864e40ebba
Kevin C. Tofel <kevin@kctofel.com>
2022-10-17 21:29:29 -0400

    initial

---
+++ joinOr.js
@@ -0,0 +1,14 @@
+// Write a function named joinOr that produces the following results:
+
+// joinOr([1, 2, 3]);               // => "1, 2, or 3"
+// joinOr([1, 2, 3], '; ');         // => "1; 2; or 3"
+// joinOr([1, 2, 3], ', ', 'and');  // => "1, 2, and 3"
+// joinOr([]);                      // => ""
+// joinOr([5]);                     // => "5"
+// joinOr([1, 2]);                  // => "1 or 2"
+
+// Use this function in the game when prompting the user to mark a square.
+
+const joinOr = (arr) => {
+
+}
